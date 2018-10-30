1f0f936a105583f06cf7725be13908b5ea2e97c7
wangpt <1315070220@qq.com>
2018-10-30 18:51:59 +0800

    add sources

---
+++ Podfile
@@ -0,0 +1,12 @@
+
+# Podfile
+platform :ios, '9.0'
+use_frameworks!
+inhibit_all_warnings!
+
+target 'TJNetWorkClient' do
+pod "AFNetworking","~> 3.1.0"
+
+end
+
+
---
+++ TJNetWorkClient/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  TJNetWorkClient
+//
+//  Created by tao on 2018/10/29.
+//  Copyright © 2018 tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AFNetworking.h"
+
+@interface ViewController : UIViewController
+
+
+@end
+
